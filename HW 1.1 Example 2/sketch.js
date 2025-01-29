371749ca1017ec1f0d93ba54a981f11cfab50502
Starlumine <159720358+Starlumine@users.noreply.github.com>
2025-01-28 23:48:58 -0600

    HW1B

---
+++ HW 1.1 Example 2/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(142, 150);
+  noStroke()
+}
+
+function draw() {
+  background(250);
+  fill(255, 0, 0, 65);
+  circle(70, 50, 70);
+  
+  fill(0, 0, 255, 90);
+  circle(45, 90, 70);
+  
+  fill(0, 255, 0, 90);
+  circle(95, 90, 70);
+}
